7acda83c8516a4b5f768c181400a4f8c0e750d00
RikutoIKuta <rikuto1999.54@gmail.com>
2026-02-25 21:28:50 +0900

    change: dbをmysqlからpostgressに変更

--- src/main/java/com/northclout/ecsite/util/DbConnection.java
+++ src/main/java/com/northclout/ecsite/util/DbConnection.java
@@ -7,13 +7,9 @@ import java.sql.SQLException;
 public final class DbConnection {
   static {
     try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      // 明示的に登録して、DriverManager が確実に検出できるようにする。
-      java.sql.DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+      Class.forName("org.postgresql.Driver");
     } catch (ClassNotFoundException e) {
-      throw new IllegalStateException("MySQL driver not found. Add mysql-connector-j to classpath.", e);
-    } catch (java.sql.SQLException e) {
-      throw new IllegalStateException("Failed to register MySQL driver.", e);
+      throw new IllegalStateException("PostgreSQL driver not found. Add org.postgresql:postgresql to classpath.", e);
     }
   }
 
